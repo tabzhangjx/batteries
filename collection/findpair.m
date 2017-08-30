c36a64509c89702f64ff32bbe042067ad3a06fb6
tabzhangjx <tabzhangjx@outlook.com>
2017-08-31 02:45:41 +0800

    files

---
+++ collection/findpair.m
@@ -0,0 +1,20 @@
+function thepair=findpair(tempcurrent,position)
+
+dis=51;
+marki=0;
+markj=0;
+
+for i=1:position
+    for j=1:position
+        if tempcurrent(i)<0&&tempcurrent(j)>0
+            if dis>distance(i,j,position)
+                dis=distance(i,j,position);
+                marki=i;
+                markj=j;
+            end
+        end
+    end
+end
+
+thepair(1)=marki;
+thepair(2)=markj;
